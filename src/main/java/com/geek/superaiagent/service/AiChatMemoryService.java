0f4083abb20d7d96801fa61753ee120fcf9942dd
Knight <liujunfeng242@gmail.com>
2026-05-17 22:21:27 +0800

    细节修改 1.注释MySQL ChatMemory相关代码，防止跟PgVector配置冲突 2.修改文件名称使其符合整体文档名称

--- src/main/java/com/geek/superaiagent/service/AiChatMemoryService.java
+++ src/main/java/com/geek/superaiagent/service/AiChatMemoryService.java
@@ -1,15 +1,15 @@
-package com.geek.superaiagent.service;
-
-import com.geek.superaiagent.entity.AiChatMemory;
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.springframework.stereotype.Service;
-
-/**
-* @author Prosper
-* @description 针对表【ai_chat_memory】的数据库操作Service
-* @createDate 2026-05-02 21:46:32
-*/
-@Service
-public interface AiChatMemoryService extends IService<AiChatMemory> {
-
-}
+//package com.geek.superaiagent.service;
+//
+//import com.geek.superaiagent.entity.AiChatMemory;
+//import com.baomidou.mybatisplus.extension.service.IService;
+//import org.springframework.stereotype.Service;
+//
+///**
+//* @author Prosper
+//* @description 针对表【ai_chat_memory】的数据库操作Service
+//* @createDate 2026-05-02 21:46:32
+//*/
+//@Service
+//public interface AiChatMemoryService extends IService<AiChatMemory> {
+//
+//}
